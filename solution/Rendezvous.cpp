a4d76d692e5eff933b47efad11b037a3d1a9a8fc
RaduM <u5yyfh@splm.siemens.com>
2021-01-25 16:49:09 +0200

    added Rendezvous solution

---
+++ solution/Rendezvous.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <thread>
+
+#include "Semaphore.h"
+
+Semaphore aArrived(0), bArrived(0);
+
+void threadA()
+{
+    std::cout << "statement a1\n"; 
+    aArrived.notify();
+    bArrived.wait();
+    std::cout << "statement a2\n"; 
+}
+
+void threadB()
+{
+    std::cout << "statement b1\n"; 
+    bArrived.notify();
+    aArrived.wait();
+    std::cout << "statement b2\n"; 
+}
+
+int main()
+{
+    auto A = std::thread(threadA);
+    auto B = std::thread(threadB);
+    A.join();
+    B.join();
+    return 0;
+}
